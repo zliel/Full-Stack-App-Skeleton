9310c0a7b9acff12f3b138ca20199bec79fb68a5
zliel <zpliel@gmail.com>
2020-09-24 17:15:13 -0700

    Added Users route

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from './core/Home'
+import Users from './user/Users'
 
 const MainRouter = () => {
     //Each Route component will have a path to a different view
@@ -8,6 +9,7 @@ const MainRouter = () => {
         <div>
             <Switch>
                 <Route exact path="/" component={Home} /> 
+                <Route path="/users" component={Users} />
             </Switch>
         </div>
     )
