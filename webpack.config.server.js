b57a4e03c09745b65e496cd7e2cc58d79fc026fd
zliel <zpliel@gmail.com>
2020-09-27 11:01:08 -0700

    Removed unnecessary import

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,15 +1,13 @@
 const path = require('path')
-const webpack = require('webpack')
 const nodeExternals = require('webpack-node-externals')
 const CURRENT_WORKING_DIR = process.cwd()
 
 const config = {
-    //explanations in webpack.config.client.js
     name: "server",
-    entry: [path.join(CURRENT_WORKING_DIR, './server/server.js') ],
+    entry: [ path.join(CURRENT_WORKING_DIR , './server/server.js') ],
     target: "node",
     output: {
-        path: path.join(CURRENT_WORKING_DIR, '/dist/'),
+        path: path.join(CURRENT_WORKING_DIR , '/dist/'),
         filename: "server.generated.js",
         publicPath: '/dist/',
         libraryTarget: "commonjs2"
@@ -20,7 +18,7 @@ const config = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: ['babel-loader']
+                use: [ 'babel-loader' ]
             },
             {
                 test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
