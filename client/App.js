6a4cf82d7e288a5cbb86410b3ed35023af01c06c
zliel <zpliel@gmail.com>
2020-09-26 20:44:30 -0700

    Implemented Server-side rendering

--- client/App.js
+++ client/App.js
@@ -6,6 +6,12 @@ import theme from './theme'
 import { hot } from 'react-hot-loader'
 
 const App = () => {
+    React.useEffect(() => {
+        const jssStyles = document.querySelector('#jss-server-side')
+        if (jssStyles) {
+            jssStyles.parentNode.removeChild(jssStyles)
+        }
+    }, [])
     return (
         <BrowserRouter>
             <ThemeProvider theme={theme}>
